a240f960e44da46cec5a2acbe9e31ee6f0fa3e77
Well192 <maxwel.paredes.l@uni.pe>
2022-05-10 00:12:29 -0500

    asignacion de vertices a las 18 piezas y en orden de turno

--- windMillPlay/sprint_1/GUI.py
+++ windMillPlay/sprint_1/GUI.py
@@ -1,57 +1,78 @@
 from tkinter import *
-from PIL import Image,ImageTk
 
-root = Tk()
-root.title("Nine men's morris")
+class Milis:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title('milis')
+        self.window.geometry("400x400")
+        self.canvas = Canvas(self.window)
+        self.cambia = True
+        self.controlador = True
+        self.count = 0
+        # primer cuadrado
+        self.canvas.create_line(30,20, 350, 20, fill='#332',width=3)
+        self.canvas.create_line(348, 20, 350,200000, fill='#332', width=3)
+        self.canvas.create_line(30,265, 350, 265, fill='#332',width=3)
+        self.canvas.create_line(30, 20, 350,200000, fill='#332', width=3)
 
-canvas = Canvas(root)
+        # segundo cuadrado
+        self.canvas.create_line(60,50, 315, 50, fill='#332',width=3)
+        self.canvas.create_line(313, 50,  313,235, fill='#332', width=3)
+        self.canvas.create_line(60,235, 315, 235, fill='#332',width=3)
+        self.canvas.create_line(60, 50, 60,235, fill='#332', width=3)
+        #tercer cuadrado
+        self.canvas.create_line(100,80, 275, 80, fill='#332',width=3)
+        self.canvas.create_line(273, 80,  273,200, fill='#332', width=3)
+        self.canvas.create_line(100,200, 275, 200, fill='#332',width=3)
+        self.canvas.create_line(100, 80, 100,200, fill='#332', width=3)
 
-# primer cuadrado
-canvas.create_line(30,20, 350, 20, fill='#332',width=3)
-canvas.create_line(348, 20, 350,200000, fill='#332', width=3)
-canvas.create_line(30,265, 350, 265, fill='#332',width=3)
-canvas.create_line(30, 20, 350,200000, fill='#332', width=3)
+        #linea horizontal 1
+        self.canvas.create_line(30,142.5, 100, 142.5, fill='#332',width=3)
+        #linea horizontal 2
+        self.canvas.create_line(348,142.5, 274.5, 142.5, fill='#332',width=3)
+        #linea vertical  1
+        self.canvas.create_line(189, 20, 189,79, fill='#332', width=3)
 
-# segundo cuadrado
-canvas.create_line(60,50, 315, 50, fill='#332',width=3)
-canvas.create_line(313, 50,  313,235, fill='#332', width=3)
-canvas.create_line(60,235, 315, 235, fill='#332',width=3)
-canvas.create_line(60, 50, 60,235, fill='#332', width=3)
-#tercer cuadrado
-canvas.create_line(100,80, 275, 80, fill='#332',width=3)
-canvas.create_line(273, 80,  273,200, fill='#332', width=3)
-canvas.create_line(100,200, 275, 200, fill='#332',width=3)
-canvas.create_line(100, 80, 100,200, fill='#332', width=3)
+        #linea vertical 2
+        self.canvas.create_line(189, 265, 189,200, fill='#332', width=3)
+        self.canvas.grid(column=0, row=0, sticky=(N, W, E, S))
+        #controlador
 
-#linea horizontal 1
-canvas.create_line(30,142.5, 100, 142.5, fill='#332',width=3)
-#linea horizontal 2
-canvas.create_line(348,142.5, 274.5, 142.5, fill='#332',width=3)
-#linea vertical  1
-canvas.create_line(189, 20, 189,79, fill='#332', width=3)
 
-#linea vertical 2
-canvas.create_line(189, 265, 189,200, fill='#332', width=3)
-#canvas.itemconfigure(id, fill='red', width=2)
-canvas.grid(column=0, row=0, sticky=(N, W, E, S))
-root.geometry("400x400")
+        self.window.bind('<Button-1>',self.posicionandoFicha)
 
-img= ImageTk.PhotoImage(Image.open("piece.png"))
-my_img = canvas.create_image(220,100,image=img)
-img2= ImageTk.PhotoImage(Image.open("piece2.png"))
-my_img2 = canvas.create_image(220,100,image=img)
-canvas.create_image(345,140,anchor=NW,image=img)
-canvas.create_image(345,140,anchor=NW,image=img2)
+        #self.window.bind('<Button-1>',self.cambiaTurno )
+    def cambiaTurno(self,e):
+        if self.cambia:
+            print(e.x,e.y)
+        else:
+            print(e.x,e.y)
+    def posicionandoFicha(self,e):
+        if self.cambia:
+            self.a = PhotoImage(file="piece.png")
+            self.canvas.create_image(e.x,e.y,image=self.a)
+            self.cambia=False
+            self.a.name = self.a.name+"1"
+            self.count += 1
 
-def move(e):
-    global img
-    img= ImageTk.PhotoImage(Image.open("piece.png"))
-    my_img = canvas.create_image(e.x,e.y,image=img)
-    img2= ImageTk.PhotoImage(Image.open("piece2.png"))
-    my_img2 = canvas.create_image(e.x,e.y,image=img2)
+        else:
+            if(self.count>16):
+                self.cambia=True
+                self.window.bind('<Button-1>',self.cambiaTurno)
+                self.a = PhotoImage(file="piece.png")
+                self.canvas.create_image(e.x,e.y,image=self.a)
+                self.a.name = self.a.name+"1"
 
-    #canvas.create_image(342,15,anchor=NW,image=img)
-    #canvas.create_image(95,195,anchor=NW,image=img)
+            else:
+                self.a= PhotoImage(file="piece2.png")
+                self.canvas.create_image(e.x,e.y,image=self.a)
+                self.cambia=True
+                self.a.name = self.a.name+"1"
+                self.count += 1
 
-canvas.bind('<B1-Motion>',move)
-root.mainloop()
+    def mainloop(self):
+        self.window.mainloop()
+
+
+milisInstance = Milis()
+milisInstance.mainloop()
