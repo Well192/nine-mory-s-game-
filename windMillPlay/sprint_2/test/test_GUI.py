4480a5fb9a391efedba095cb92a3c281ca868643
fiorellamr <fiorella.meza.r@uni.pe>
2022-05-29 18:48:55 -0500

    test volada parte 1

--- windMillPlay/sprint_2/test/test_GUI.py
+++ windMillPlay/sprint_2/test/test_GUI.py
@@ -1,25 +1,27 @@
 import unittest
-from windMillPlay.sprint_2.production.GUI import WindMillPlay
+from unittest import TestCase
+from windMillPlay.sprint_2.production.GUI import *
+from windMillPlay.sprint_2.production.Data import *
 
-
-class TestWindMillPlay(unittest.TestCase):
-
-    gameGUI = WindMillPlay()
-
+class TestWindMillPlay(TestCase):
     def test_create_board(self):
-        self.assertEqual(True, self.gameGUI.state)
+        gameGUI = WindMillPlay()
+        self.assertEqual(True, gameGUI.state)
 
     def test_validate_positive_position(self):
+        gameGUI = WindMillPlay()
         #Insertamos posiciones cercanas a los vértices
-        self.assertEqual(True, self.gameGUI.validatePosition(116, 116, 377, 116))
+        self.assertEqual(True, gameGUI.validatePosition(116, 116, 377, 116))
 
     def test_not_validate_positive_position(self):
+        gameGUI = WindMillPlay()
         #Insertamos posiciones cercanas a los vértices
-        self.assertFalse(False, self.gameGUI.validatePosition(30, 377, 116, 116))
+        self.assertFalse(False,gameGUI.validatePosition(30,377,116, 116))
 
     def test_not_validate_negative_position(self):
+        gameGUI = WindMillPlay()
         #Insertamos posiciones cercanas a los vértices
-        self.assertFalse(False, self.gameGUI.validatePosition(116, 116, -45, -30))
+        self.assertFalse(False,gameGUI.validatePosition(116, 116,-45,-30))
 
 if __name__ == '__main__':
     unittest.main()
--- windMillPlay/sprint_2/test/test_volada.py
+++ windMillPlay/sprint_2/test/test_volada.py
@@ -4,7 +4,43 @@ from windMillPlay.sprint_2.production.Logic import WindMillPlayGame
 from windMillPlay.sprint_2.production.Data import *
 
 class TestWindMillPlayGame(TestCase):
-    def test_volada(self):
+    def test_volada_case5(self):
         game =  WindMillPlayGame()
-        game.volada()
+        position_list[1].ficha = 1
+        position_list[1].empty = False
+        position_list[9].ficha = 3
+        position_list[9].empty = False
+        position_list[17].ficha = 5
+        position_list[17].empty = False
+        self.assertEqual(True, game.volada(5))
+
+    def test_volada_case5_false(self):
+        game =  WindMillPlayGame()
+        position_list[1].ficha = 1
+        position_list[1].empty = False
+        position_list[9].ficha = 3
+        position_list[9].empty = False
+        position_list[17].ficha = 6
+        position_list[17].empty = False
+        self.assertEqual(False, game.volada(6))
+
+    def test_volada_case6(self):
+        game =  WindMillPlayGame()
+        position_list[1].ficha = 1
+        position_list[1].empty = False
+        position_list[9].ficha = 3
+        position_list[9].empty = False
+        position_list[17].ficha = 5
+        position_list[17].empty = False
+        self.assertEqual(True, game.volada(5))
+
+    def test_volada_case6_false(self):
+        game =  WindMillPlayGame()
+        position_list[1].ficha = 1
+        position_list[1].empty = False
+        position_list[9].ficha = 3
+        position_list[9].empty = False
+        position_list[17].ficha = 5
+        position_list[17].empty = False
+        self.assertEqual(False, game.volada(5))
 
