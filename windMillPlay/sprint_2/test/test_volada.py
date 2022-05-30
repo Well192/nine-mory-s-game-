c86c1ea6086167da949baf304409a686a86cf12e
Well192 <maxwel.paredes.l@uni.pe>
2022-05-29 19:37:48 -0500

    pruebas unitarias para la volada de fichas blancas

--- windMillPlay/sprint_2/test/test_volada.py
+++ windMillPlay/sprint_2/test/test_volada.py
@@ -4,8 +4,45 @@ from windMillPlay.sprint_2.production.Logic import WindMillPlayGame
 from windMillPlay.sprint_2.production.Data import *
 
 class TestWindMillPlayGame(TestCase):
+
+    def test_volada_case1(self):
+        game =  WindMillPlayGame()
+
+        position_list[0].ficha = 1
+        position_list[0].empty = False
+        position_list[6].ficha = 3
+        position_list[6].empty = False
+        position_list[7].ficha = 5
+        position_list[7].empty = False
+        self.assertEqual(True, game.volada(5))
+
+    def test_volada_case1_2(self):
+
+        game =  WindMillPlayGame()
+
+        position_list[8].ficha = 1
+        position_list[8].empty = False
+        position_list[14].ficha = 3
+        position_list[14].empty = False
+        position_list[15].ficha = 5
+        position_list[15].empty = False
+        self.assertEqual(True, game.volada(5))
+
+    def test_volada_case1_3(self):
+
+        game =  WindMillPlayGame()
+
+        position_list[16].ficha = 1
+        position_list[16].empty = False
+        position_list[22].ficha = 3
+        position_list[22].empty = False
+        position_list[23].ficha = 5
+        position_list[23].empty = False
+        self.assertEqual(True, game.volada(5))
+
     def test_volada_case5(self):
         game =  WindMillPlayGame()
+
         position_list[1].ficha = 1
         position_list[1].empty = False
         position_list[9].ficha = 3
@@ -16,6 +53,7 @@ class TestWindMillPlayGame(TestCase):
 
     def test_volada_case5_false(self):
         game =  WindMillPlayGame()
+
         position_list[1].ficha = 1
         position_list[1].empty = False
         position_list[9].ficha = 3
