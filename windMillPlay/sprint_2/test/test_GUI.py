90b64a391f4a269dadd5e06cc4334c61e92942fd
ARMANDO\Armando <armandolluengallardo@gmail.com>
2022-05-29 19:42:10 -0500

    test gui 3.0

--- windMillPlay/sprint_2/test/test_GUI.py
+++ windMillPlay/sprint_2/test/test_GUI.py
@@ -4,24 +4,24 @@ from windMillPlay.sprint_2.production.GUI import *
 from windMillPlay.sprint_2.production.Data import *
 
 class TestWindMillPlay(TestCase):
+
+    gameGUI = WindMillPlay()
+
     def test_create_board(self):
-        gameGUI = WindMillPlay()
-        self.assertEqual(True, gameGUI.state)
+
+        self.assertEqual(True, self.gameGUI.state)
 
     def test_validate_positive_position(self):
-        gameGUI = WindMillPlay()
         #Insertamos posiciones cercanas a los vértices
-        self.assertEqual(True, gameGUI.validatePosition(116, 116, 377, 116))
+        self.assertEqual(True, self.gameGUI.validatePosition(116, 116, 377, 116))
 
     def test_not_validate_positive_position(self):
-        gameGUI = WindMillPlay()
         #Insertamos posiciones cercanas a los vértices
-        self.assertFalse(False,gameGUI.validatePosition(30,377,116, 116))
+        self.assertFalse(False, self.gameGUI.validatePosition(30,377,116, 116))
 
     def test_not_validate_negative_position(self):
-        gameGUI = WindMillPlay()
         #Insertamos posiciones cercanas a los vértices
-        self.assertFalse(False,gameGUI.validatePosition(116, 116,-45,-30))
+        self.assertFalse(False, self.gameGUI.validatePosition(116, 116,-45,-30))
 
 if __name__ == '__main__':
     unittest.main()
